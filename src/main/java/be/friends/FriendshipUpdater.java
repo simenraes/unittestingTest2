2639a5eb7d1c88b0ebec2b33bf9b0b097cf9d841
simenraes <simieraes@hotmail.com>
2021-10-21 16:38:05 +0200

    het lukte niet goed

--- src/main/java/be/friends/FriendshipUpdater.java
+++ src/main/java/be/friends/FriendshipUpdater.java
@@ -8,25 +8,44 @@ public class FriendshipUpdater {
         this.friends = friends;
     }
 
+    public void updateFriendShipLevel(Friend friend) {
+        if (friend.nrOfStars == 0 && friend.didSomeInteractionToday)
+            friend.friendshipLevel -= 1;
+
+        if (friend.didSomeInteractionToday && !friend.isLucky)
+            friend.friendshipLevel += 1;
+        else if (friend.didSomeInteractionToday)
+            friend.friendshipLevel += 5;
+
+        if (friend.friendshipLevel == 10 && friend.nrOfStars == 1)
+            friend.friendshipLevel = 0;
+        if (friend.friendshipLevel > 30)
+            friend.friendshipLevel= 0;
+
+        if (friend.friendshipLevel == 0 && friend.nrOfStars == 1)
+            friend.nrOfStars += 1;
+    }
+
     public void update() {
         for (Friend friend : friends) {
             boolean wasLucky = !friend.isLucky;
             if (friend.isLucky && friend.didSomeInteractionToday) {
                 friend.isLucky = false;
             }
-            if (!wasLucky)
-                friend.friendshipLevel += 4;
+
+            updateFriendShipLevel(friend);
             if (friend.didSomeInteractionToday) {
+
                 if (friend.nrOfStars == 0)
                     friend.nrOfStars++;
-                else {
+               else {
                     if (friend.friendshipLevel == 9) {
-                        if (friend.nrOfStars == 1) {
-                            friend.friendshipLevel = -1;
-                            friend.nrOfStars = 2;
-                        }
+                       if (friend.nrOfStars == 1) {
+//                           friend.friendshipLevel = -1;
+//                           friend.nrOfStars = 2;
+                      }
                     }
-                    friend.friendshipLevel++;
+
                 }
                 if (friend.nrOfStars != 3 && friend.friendshipLevel == 30) {
                     friend.friendshipLevel = 0;
@@ -38,4 +57,8 @@ public class FriendshipUpdater {
             }
         }
     }
+
+
 }
+
+
